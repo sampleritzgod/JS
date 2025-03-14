ea58c2dcc3df62ef1e8c682ed2a4abe514e13179
abhays09 <106572999+sampleritzgod@users.noreply.github.com>
2025-03-14 13:19:29 +0000

    variable

---
+++ 01_basic/variable.js
@@ -0,0 +1,8 @@
+const accountID = 1234;
+let emailID = "abhay005@gamuil.com";
+// var  - do not used 
+let userCity = "rajkot";
+let password = "4FMxABHAY009@";
+let userSTate;
+
+console.table([accountID, emailID, userCity, userSTate, password]);
