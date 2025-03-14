471d2de8d2c46e3132c6dc8f45b814e0dbc107da
abhays09 <106572999+sampleritzgod@users.noreply.github.com>
2025-03-14 13:38:38 +0000

    dataType

---
+++ 01_basic/dataType.js
@@ -0,0 +1,13 @@
+"use strict"; // - trect all JS code as newar version   
+// null  -  stand alone value representing empty value  
+// undefined - value not defined 
+//  int 
+// string 
+// number
+// bigint
+// symbol  -> unique  { primitive data type}
+
+// object
+// typeof() - type of data
+// array
+// null is object 
