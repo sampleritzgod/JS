67fa360d6979c12e3b62b5705bb930a236ebf7fa
abhays09 <106572999+sampleritzgod@users.noreply.github.com>
2025-03-15 08:39:31 +0000

    changes

--- 01_basic/summery.js
+++ 01_basic/summery.js
@@ -29,3 +29,24 @@ let User1 = {
     role: "assulter",
 }
 console.log(User1);
+
+// stack (primitive) heap (non-primitive)
+// in stack we get copy of output ya sometinng but in heap we get real / original value (memory)
+
+let YouTube = "AbhayBGMI";
+let AnotherChannel = YouTube;
+AnotherChannel = "AbhayOG"
+console.log(AnotherChannel);
+console.log(YouTube);
+
+let UserOne = {
+    Email: "user123@gmail.com",
+    Id: "123@ac",
+    age: 22,
+    passowrd: "QWx23@91"
+}
+let UserTwo = UserOne;
+console.log(UserTwo);
+console.log(typeof UserOne);
+UserOne.Email = "Tanisha21@microsoft.com"
+console.log( UserOne);
