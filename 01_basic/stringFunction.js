c696cbb0091bd98c3f2d20b4e13c641a8cdf4e9e
abhays09 <106572999+sampleritzgod@users.noreply.github.com>
2025-03-15 09:17:53 +0000

    StringFunction

---
+++ 01_basic/stringFunction.js
@@ -0,0 +1,22 @@
+let name = "abhay";
+let RepoCount = 12 ;
+console.log(`in GitHub ${name} have a ${RepoCount} repository `);
+// string interpolation
+const NewGame = new String('Abhay09-shrushti9');
+console.log(NewGame[0]);
+console.log(NewGame.__proto__);
+//
+console.log(NewGame.length);
+console.log(NewGame.toUpperCase());
+console.log(NewGame.charAt(2));
+console.log(NewGame.indexOf('a'));
+console.log(NewGame.substring(0,3));
+console.log(NewGame.slice(-2, 4));
+console.log(NewGame.trim());
+
+const urL = "https://AnewGen@software%20.com/page";
+console.log(urL.replace('%20', '-'));
+console.log(urL.includes('AnewGen'));
+console.log(NewGame.split('-'));
+
+
